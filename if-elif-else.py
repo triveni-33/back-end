204ddc1a17818e984981080398a68939b7a17801
triveni-33 <boyapatitriveni5@gmail.com>
2026-02-23 18:51:27 +0530

    Added new file

---
+++ if-elif-else.py
@@ -0,0 +1,74 @@
+"""time=int(input("enter the time"))
+if time==9:
+    print("attend python class")
+else:
+    print(" attend pratice session")"""
+
+# to check whether num is even or not
+"""num=int(input("Enter number"))
+if num%2==0:
+    print("even")
+else:
+    print("odd")"""
+
+
+#if-elif-else:
+#checking whether a number is positive,negative or zero
+"""
+num=int(input("enter num"))
+if num>0:
+    print("positive number")
+elif num<0:
+    print("negative number")
+else:
+    print("zero")
+"""
+
+#checking which number is greater between them
+"""
+x = int(input("Enter first number"))
+y = int(input("Enter second number"))
+
+if x > y:
+    print("x is greater")
+elif y > x:
+    print("y is greater ")
+else:
+    print("Both numbers are equal")
+"""
+
+#checking leapyear  or not
+"""
+year = int(input("Enter a year"))
+
+if (year % 4 == 0 and year % 100 != 0):
+    print("It is a Leap Year")
+elif(year % 400 == 0):
+    print("its a leap year")
+else:
+    print("It is Not a Leap Year")
+"""
+#checking the traffic signals
+"""
+signal=input("Enter the signal")
+if signal=="red":
+    print("stop")
+elif signal=="green":
+    print("go")
+elif signal=="orange":
+    print("ready")
+else:
+    print("invalid signal")
+"""
+#checking the grades
+"""
+marks=int(input("Enter num"))
+if marks>90:
+    print("A grade")
+elif marks>80 and marks<=90:
+    print("B grade")
+elif marks>=75 and marks==80:
+    print("c grade")
+else:
+    print("fail")
+"""
